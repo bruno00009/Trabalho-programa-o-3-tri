c62d3271287149089cc13bef699552c9e6f1b019
bruno00009 <161394551+bruno00009@users.noreply.github.com>
2024-11-11 14:30:42 -0300

    Add files via upload

---
+++ flashcard-aula_10/perguntas.js
@@ -0,0 +1,28 @@
+criaCartao(
+    'MATEMATICA II PROGRAMAÇÃO',
+    'Meu trabalho é?',
+    'Auxiliar Administrativo'
+)
+
+criaCartao(
+    'MATEMATICA II PROGRAMAÇÃO',
+    ' Quem sou eu?',
+    'Bruno Oliveira'
+)
+
+criaCartao(
+    'MATEMATICA II PROGRAMAÇÃO',
+    'O que eu mais gosto no meu trabalho é?',
+    'Projetos e compras'
+)
+
+criaCartao(
+    'MATEMATICA II PROGRAMAÇÃO',
+    'Nos finais de semana prefiro? ',
+    'Ficar com a minha namorada'
+)
+criaCartao(
+    'MATEMATICA II PROGRAMAÇÃO',
+    'Meu esporte favorito é? ',
+    'Futsal e futebol'
+)
